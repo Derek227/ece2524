434c9bfc57e38dd2942256ccd63c695d182c192e
Derek Loftis <derek227@vt.edu>
2012-03-21 09:14:08 -0400

    Modified countFiles.py
    countFiles.py
    -------------
    Second version, runs as a standalone program, but has the counting part in a function
    
    Future changes : None planed so far.

--- countFiles.py
+++ countFiles.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 import os
+
+def countFiles(path):
+	total=0
+	for subdir, dir, files in os.walk(path):
+		for file in files:
+			total += 1
+	return (total)
+
 from sys import stdout,stdin,argv
 
-total=0
 
-for subdir, dir, files in os.walk(argv[1]):
-	for file in files:
-		total += 1
-
+total = countFiles(argv[1])
 stdout.write('Total files: %d\n' % total)	
